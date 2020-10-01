ea7ccecaf8a5256f7a3624b8ab1d6f25f10b0dbe
mitchellburley <mitch--is@hotmail.com>
2020-10-01 14:00:09 +1000

    test

---
+++ github-finder/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+
+class App extends Component {
+
+  render() {
+    return (
+      <nav className="navbar bg-primary">
+        <Navbar />
+      </nav>
+    );
+  }
+}
+
+export default App;
---
+++ github-finder/src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h1><i className="fab fa-github"></i>GitHub Finder</h1>
+            </div>
+        )
+    }
+}
+
+export default Navbar
